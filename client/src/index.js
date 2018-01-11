18de8770c522db7dd565bb695cc2e478b898a153
Guillaume Buchou <guillaume.buchou@sewan.fr>
2018-01-11 20:33:03 +0100

    inch allah

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(reduxThunk)));
 
 
-var audioContextHandler = new AudioContextHandler(store);
+//var audioContextHandler = new AudioContextHandler(store);
 
 
 ReactDOM.render(
