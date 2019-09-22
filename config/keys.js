5e9c5f31c4defe3385bc74eb9cc55062b5c7dcc9
gbuchou <gbuchou@tradelab.fr>
2019-09-22 17:02:37 +0200

    [UPDATE] use keroku key and auto deploy

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-    facebookClientID:'1625179270850640',
-    facebookClientSecret:'a6383bca49348dca2fe5ea4559ca4c98',
-    mongoURI: 'mongodb://colaps_admin:XbY66ZqsuiTBlsnraWfC@ds251747.mlab.com:51747/colaps',
-    cookieKey: 'sjkfdfjdjkfhkjdhfkjdfhkdfjsnsefdfkfkj'
+    facebookClientID:process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret:process.env.FACEBOOK_CLIENT_SECRET,
+    mongoURI: process.env.MONGODB_URI,
+    cookieKey:process.env.COOKIE_KEY
 
 };
